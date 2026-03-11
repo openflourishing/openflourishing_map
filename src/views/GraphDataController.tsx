d20dec3d401e4ef6f2cff05a945f72371f3852c9
pythoro <pythoro@mindquip.com>
2026-03-11 20:35:27 +1300

    [IMP] Add contexts.

--- src/views/GraphDataController.tsx
+++ src/views/GraphDataController.tsx
@@ -11,9 +11,32 @@ const GraphDataController: FC<PropsWithChildren<{ filters: FiltersState }>> = ({
    * Apply filters to graphology:
    */
   useEffect(() => {
-    const { clusters, tags } = filters;
-    graph.forEachNode((node, { cluster, tag }) => {
-      graph.setNodeAttribute(node, "hidden", !clusters[cluster] || !tags[tag]);
+    const { clusters, tags, contexts } = filters;
+    const contextKeys = Object.keys(contexts);
+    const hasAnyContexts = contextKeys.length > 0;
+    
+    graph.forEachNode((node, nodeData) => {
+      const { cluster, tag } = nodeData;
+      
+      // Check cluster and tag filters
+      let isVisible = clusters[cluster] && tags[tag];
+      
+      // Check context filters
+      // If there are no contexts in the filter at all (uncheck all), hide the node
+      // If there are contexts in the filter, show node if any of its contexts match
+      if (isVisible) {
+        if (hasAnyContexts) {
+          // At least one context is checked - show if node has a matching context
+          const nodeContexts = nodeData.contexts || [];
+          const hasMatchingContext = nodeContexts.some((ctx: string) => contexts[ctx]);
+          isVisible = hasMatchingContext;
+        } else {
+          // No contexts are checked (uncheck all) - hide all nodes
+          isVisible = false;
+        }
+      }
+      
+      graph.setNodeAttribute(node, "hidden", !isVisible);
     });
   }, [graph, filters]);
 
